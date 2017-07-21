18acfbb0f6da7f90ab568924b84a64f534d80586
gboeing <gboeing@berkeley.edu>
2017-07-21 14:24:05 -0700

    add tests

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,13 @@
-import choicemodels as cm
+def test_import():
+    import choicemodels
+
+def test_simple_estimation():
+    import choicemodels
+    import numpy as np
+    import pandas as pd
+    from collections import OrderedDict
+    endog = np.random.randint(2, size=50)
+    exog = np.random.rand(50, 5)
+    m = choicemodels.Logit(endog, exog)
+    results = m.fit()
+    results.summary()
