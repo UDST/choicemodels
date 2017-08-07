e1e32cd41a679fa8cd2135a5a8f558972e634b67
Paul Waddell <paul.waddell@gmail.com>
2017-08-06 19:21:19 -0700

    Relaxed the somewhat arbitrary cap on the range of coefficients, which was set to -3 to 3, to a -1000 to 1000 range to be less restrictive.

--- choicemodels/mnl.py
+++ choicemodels/mnl.py
@@ -587,7 +587,7 @@ def mnl_simulate(data, coeff, numalts, GPU=False, returnprobs=True):
     return choices.get_mat()
 
 
-def mnl_estimate(data, chosen, numalts, GPU=False, coeffrange=(-3, 3),
+def mnl_estimate(data, chosen, numalts, GPU=False, coeffrange=(-1000, 1000),
                  weights=None, lcgrad=False, beta=None):
     """
     Calculate coefficients of the MNL model.
