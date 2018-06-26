3f19efd924da3a9d3d25ec6a0e1e506b7c83656b
gboeing <gboeing@berkeley.edu>
2018-06-26 09:52:33 -0700

    add function to get nodes from subgraphs with distance bands

--- choicemodels/tools/net_dist.py
+++ choicemodels/tools/net_dist.py
@@ -24,3 +24,15 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+
+def get_subgraph_nodes(G, node, dists):
+    
+    subgraph_nodes = {}
+    
+    for dist1, dist2 in pairwise(dists):
+        G_sub = nx.ego_graph(G, node, radius=dist2, distance='length', center=True, undirected=False)
+        subgraph_nodes[(dist1, dist2)] = set(G_sub.nodes())
+    
+    return subgraph_nodes
