7c6ea8cf6479ad701f7e09faccdcd4ed164d320b
gboeing <gboeing@berkeley.edu>
2018-06-26 09:53:26 -0700

    add function to get nodes in bands

--- choicemodels/tools/net_dist.py
+++ choicemodels/tools/net_dist.py
@@ -36,3 +36,23 @@ def get_subgraph_nodes(G, node, dists):
         subgraph_nodes[(dist1, dist2)] = set(G_sub.nodes())
     
     return subgraph_nodes
+
+
+
+def get_band_nodes(dists, subgraph_nodes):
+    
+    band_nodes = {}
+    pairwise_dists = list(pairwise(dists))
+    
+    for idx1, dist_pair1 in reversed(list(enumerate(pairwise_dists))):
+
+        if idx1 < 1:
+            break
+
+        dist_pair2 = pairwise_dists[idx1 - 1]
+        band_nodes[dist_pair1] = subgraph_nodes[dist_pair1] - subgraph_nodes[dist_pair2]
+
+    band_nodes[pairwise_dists[0]] = subgraph_nodes[pairwise_dists[0]]
+    
+    return band_nodes
+    
