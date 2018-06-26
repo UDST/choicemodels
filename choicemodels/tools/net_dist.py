e5c6a539c1d3c4e55c9b9ea279d95050357edb24
gboeing <gboeing@berkeley.edu>
2018-06-26 09:51:39 -0700

    add pairwise iterator function

--- choicemodels/tools/net_dist.py
+++ choicemodels/tools/net_dist.py
@@ -6,3 +6,21 @@ network nodes are within various distance bands of other network nodes
 
 
 
+def pairwise(iterable):
+    """
+    Iterate through a list, pairwise.
+
+    Parameters
+    ----------
+    iterable : list-like
+        the list-like object to iterate through pairwise
+
+    Returns
+    -------
+    zip
+        a zipped iterable of pairwise tuples
+    """
+
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
