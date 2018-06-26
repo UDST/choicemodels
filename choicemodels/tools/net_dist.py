e2770536dae2aefd74d7a0f31cd3d176f31d19d6
gboeing <gboeing@berkeley.edu>
2018-06-26 09:57:56 -0700

    add docstring

--- choicemodels/tools/net_dist.py
+++ choicemodels/tools/net_dist.py
@@ -27,12 +27,35 @@ def pairwise(iterable):
 
 
 
-def get_subgraph_nodes(G, node, dists):
-    
+def get_subgraph_nodes(G, node, dists, weight='length'):
+    """
+    Get nodes in subgraphs some distance from reference node.
+
+    Parameters
+    ----------
+    G : networkx MultiDiGraph
+        the street network graph
+    node : int
+    	the OSM ID of the reference node around which to
+    	induce subgraphs
+    dists : list
+    	list of distances at which to induce subgraphs around
+    	the reference node
+    weight : string
+    	the name of the edge attribute to weight shortest
+    	path distance calculation by
+
+    Returns
+    -------
+    subgraph_nodes : dict
+        a dictionary keyed by (dist1, dist2) with value of
+        set of node IDs in that subgraph
+    """
+
     subgraph_nodes = {}
     
     for dist1, dist2 in pairwise(dists):
-        G_sub = nx.ego_graph(G, node, radius=dist2, distance='length', center=True, undirected=False)
+        G_sub = nx.ego_graph(G, node, radius=dist2, distance=weight, center=True, undirected=False)
         subgraph_nodes[(dist1, dist2)] = set(G_sub.nodes())
     
     return subgraph_nodes
@@ -40,7 +63,20 @@ def get_subgraph_nodes(G, node, dists):
 
 
 def get_band_nodes(dists, subgraph_nodes):
-    
+    """
+    Iterate through a list, pairwise.
+
+    Parameters
+    ----------
+    iterable : list-like
+        the list-like object to iterate through pairwise
+
+    Returns
+    -------
+    zip
+        a zipped iterable of pairwise tuples
+    """
+
     band_nodes = {}
     pairwise_dists = list(pairwise(dists))
     
@@ -55,4 +91,3 @@ def get_band_nodes(dists, subgraph_nodes):
     band_nodes[pairwise_dists[0]] = subgraph_nodes[pairwise_dists[0]]
     
     return band_nodes
-    
