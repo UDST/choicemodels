13c7ede4642a00d321e8c150fb19a8540e8c0df2
Sam Maurer <maurer@berkeley.edu>
2018-08-15 16:45:27 -0700

    Cleaning up deprecated code

--- choicemodels/tools/interaction.py
+++ choicemodels/tools/interaction.py
@@ -1,15 +1,12 @@
 """
-Utilities for making merged interaction tables of choosers and the alternatives from which 
-they are choosing. Used for location choice models.
+Utilities for generating merged tables of choosers and alternatives, including extensive
+sampling functionality.
 
 """
-import logging
 import random
 
 import numpy as np
 import pandas as pd
-
-from . import pmat
 
 
 class MergedChoiceTable(object):
@@ -50,7 +47,7 @@ class MergedChoiceTable(object):
         efficient, so setting replace=False may have performance implications if there are
         very large numbers of observations or alternatives.        
     
-    weights : str, pandas.Series, or callable, optional
+    weights : str, pandas.Series, or callable, optional (CALLABLE NOT YET IMPLEMENTED)
         Numerical weights to apply when sampling alternatives. If str, interpreted as a 
         column from the alternatives table. If Series, it can contain either (a) one 
         weight for each alternative or (b) one weight for each combination of observation 
@@ -60,15 +57,19 @@ class MergedChoiceTable(object):
         alternatives table. If callable, it should accept two arguments (obs_id, alt_id) 
         and return the corresponding weight.
     
-    availability : NOT YET IMPLEMENTED
+    availability : pandas.Series or callable, optional (NOT YET IMPLEMENTED)
         Binary representation of the availability of alternatives. Specified and applied 
         similarly to the weights.
     
+    interaction_terms : NOT YET IMPLEMENTED
+        Additional columns of interaction terms whose values depend on the combination of 
+        observation and the alternative, to be joined onto the final data table. Specified 
+        similarly to the weights.
+            
     random_state : NOT YET IMPLEMENTED
         Representation of random state, for replicability of the sampling.
 
     """
-
     def __init__(self, observations, alternatives, chosen_alternatives=None,
                  sample_size=None, replace=True, weights=None, random_state=None):
         
@@ -161,9 +162,74 @@ class MergedChoiceTable(object):
         return df
 
     
+    def _get_availability(self, obs_id):
+        """
+        Get alternative availability for a single observation id. For now, this just 
+        checks whether the chosen alternative is known and if so makes it unavailable.
+        
+        Parameters
+        ----------
+        observation_id : value from index of self.observations
+        
+        Expected class parameters
+        -------------------------
+        self.alternatives : pd.DataFrame
+        self.chosen_alternatives : pd.Series or None
+        
+        Returns
+        -------
+        list of booleans
+        
+        """
+        a = np.repeat(True, self.alternatives.shape[0])
+        
+        if (self.chosen_alternatives is not None):
+            a = (self.alternatives.index != self.chosen_alternatives[obs_id])
+
+        return a        
+    
+    
+    def _get_weights(self, obs_id):
+        """
+        Get sampling weights corresponding to a single observation id.
+        
+        Parameters
+        ----------
+        observation_id : value from index of self.observations
+        
+        Expected class parameters
+        -------------------------
+        self.weights : pd.Series, callable, or None
+        self.chosen_alternatives : pd.Series or None
+        self.weights_1d : boolean
+        self.weights_2d : boolean
+        
+        Returns
+        -------
+        pd.Series of weights
+        
+        """
+        if (self.weights is None):
+            return
+            
+        elif (callable(self.weights)):
+            # TO DO - implement
+            pass
+        
+        elif (self.weights_1d == True):
+            return self.weights
+        
+        elif (self.weights_2d == True):
+            return self.weights.loc[obs_id]
+        
+        else:
+            raise ValueError  # unexpected inputs
+                    
+
     def _build_table(self):
         """
-        Build and return the merged choice table.
+        Build and return the merged choice table. This handles all cases where sampling
+        is performed.
         
         Expected class parameters
         -------------------------
@@ -250,271 +316,65 @@ class MergedChoiceTable(object):
         return df
         
     
-    def _get_weights(self, obs_id):
-        """
-        Get sampling weights corresponding to a single observation id.
-        
-        Parameters
-        ----------
-        observation_id : value from index of self.observations
-        
-        Expected class parameters
-        -------------------------
-        self.weights : pd.Series, callable, or None
-        self.chosen_alternatives : pd.Series or None
-        self.weights_1d : boolean
-        self.weights_2d : boolean
-        
-        Returns
-        -------
-        pd.Series of weights
-        
-        """
-        if (self.weights is None):
-            return
-            
-        elif (callable(self.weights)):
-            # TO DO - implement
-            pass
-        
-        elif (self.weights_1d == True):
-            return self.weights
-        
-        elif (self.weights_2d == True):
-            return self.weights.loc[obs_id]
-        
-        else:
-            raise ValueError  # unexpected inputs
-                    
-
-    def _get_availability(self, obs_id):
-        """
-        Get alternative availability for a single observation id. For now, this just 
-        checks whether the chosen alternative is known and if so makes it unavailable.
-        
-        Parameters
-        ----------
-        observation_id : value from index of self.observations
-        
-        Expected class parameters
-        -------------------------
-        self.alternatives : pd.DataFrame
-        self.chosen_alternatives : pd.Series or None
-        
-        Returns
-        -------
-        list of booleans
-        
-        """
-        a = np.repeat(True, self.alternatives.shape[0])
-        
-        if (self.chosen_alternatives is not None):
-            a = (self.alternatives.index != self.chosen_alternatives[obs_id])
-
-        return a        
-    
-    
-    def to_frame(self):
-        return self._merged_table
-
-
-class MergedChoiceTable_deprecated(object):
-    """
-    Generates a merged long-format table of choosers and alternatives, for discrete choice
-    model estimation or simulation. 
-    
-    Attributes that vary based on interaction between the choosers and alternatives 
-    (distance, for example) will need to be added in post-processing. 
-    
-    Reserved column names: 'chosen', 'join_index', 'observation_id'.
-    
-    Parameters
-    ----------
-    observations : pandas.DataFrame
-        Table with one row for each choice scenario, with unique ID's in the index field.
-        Additional columns can contain fixed attributes of the choosers. Best not to have
-        a column with the same name as the index field of the 'alternatives' table,
-        because these will clash when the tables are merged. (If you have one, it will be
-        used to identify chosen alternatives if needed, and then dropped.)
-        [TO DO: check that this is handled correctly.]
-
-    alternatives : pandas.DataFrame
-        Table with one row for each alternative, with unique ID's in the index field.
-        Additional columns can contain fixed attributes of the alternatives.
-
-    chosen_alternatives : str or list or pandas.Series, optional
-        List of the alternative ID selected in each choice scenario. (This is required for
-        preparing estimation data, but not for simulation data.) If str, interpreted as a
-        column name from the observations table. If list, must be in the same order as the
-        observations. The column will be dropped from the merged table and replaced with a
-        binary column named 'chosen'.
-
-    sample_size : int, optional
-        Number of alternatives available for each chooser. These will be sampled randomly.
-        If 'None', all of the alternatives will be available for each chooser.
-
-    weights : str, pandas.Series, or callable, optional
-        Weights to apply when sampling alternatives. (A) One weight per alternative. If 
-        str, interpreted as a column name from the alternatives table. If Series, it 
-        should have the same length as the alternatives table and its index should align.
-        (B) One weight per combination of observation and alternative. If Series, it
-        should have one row per combination of observation id and alternative id, with
-        the first index level corresponding to the former and the second index level
-        to the latter. If callable, it should accept two arguments in the form of 
-        `observation_id`, `alternative_id` and return the corresponding weight.
-
-    """
-    def __init__(self, observations, alternatives, chosen_alternatives=None,
-                 sample_size=None, weights=None):
-
-        # TO DO: implement case where sample_size = None
-        # TO DO: implement case where chosen_alternatives is a string (might be nice to
-        #        drop it from the merged data table, to keep things clean?)
-        # TO DO: implement case where chosen_alternatives = None
-
-        alts, merged, chosen = mnl_interaction_dataset(observations, alternatives,
-                                                       sample_size, chosen_alternatives)
-
-        # Convert the representation of chosen alternatives to a column in table
-        merged['chosen'] = np.reshape(chosen.astype(int), (merged.shape[0], 1))
-
-        # Label the observation id  [TO DO: would be nice to keep original name]
-        merged = merged.rename(columns = {'join_index': 'observation_id'})
-
-        # Store the alternative id
-        self._alternative_id_col = merged.index.name
-
-        if (self._alternative_id_col in merged.columns):
-            # if there's an existing column with same name, drop it
-            merged = merged.drop(self._alternative_id_col, axis=1)
-
-        merged = merged.reset_index()  # save as column
-
-        self._merged_table = merged
-        return
-
     def to_frame(self):
         """
         Long-format DataFrame of the merged table. The rows representing alternatives for
-        a particular chooser are contiguous, with the chosen alternative listed first.
+        a particular chooser are contiguous, with the chosen alternative listed first if
+        applicable. (Unless no sampling is performed, in which case the alternatives are
+        listed in order.) The DataFrame includes a two-level MultiIndex. The first level
+        corresponds to the index of the observations table and the second level to the 
+        index of the alternatives table. 
+        
+        Returns
+        -------
+        pandas.DataFrame
 
         """
         return self._merged_table
 
+    
     @property
     def observation_id_col(self):
         """
-        Name of column in the merged table containing the observation id. Values will
-        match the index of the 'choosers' table, but for now the column name is reset.
+        Name of column in the merged table containing the observation id. Name and values 
+        will match the index of the 'choosers' table.
+        
+        Returns
+        -------
+        str
 
         """
-        return 'observation_id'
+        return self.observations.index.name
 
+    
     @property
     def alternative_id_col(self):
         """
         Name of column in the merged table containing the alternative id. Name and values
-        will match the index of the 'alternatives' table,
+        will match the index of the 'alternatives' table.
+
+        Returns
+        -------
+        str
 
         """
-        return self._alternative_id_col
+        return self.alternatives.index.name
 
+    
     @property
     def choice_col(self):
         """
         Name of the generated column containing a binary representation of whether each
         alternative was chosen in the given choice scenario.
 
+        Returns
+        -------
+        str or None
+
         """
-        return 'chosen'
+        if (self.chosen_alternatives is not None):
+            return 'chosen'
+        
+        else:
+            return None
 
-
-"""
-#############################
-ORIGINAL FUNCTION DEFINITIONS
-#############################
-
-"""
-
-logger = logging.getLogger(__name__)
-GPU = False
-
-
-def enable_gpu():
-    global GPU
-    GPU = 1
-    pmat.initialize_gpu()
-
-
-# TODO: split this out into separate functions for estimation
-# and simulation.
-def mnl_interaction_dataset(choosers, alternatives, SAMPLE_SIZE,
-                            chosenalts=None):
-    logger.debug((
-        'start: compute MNL interaction dataset with {} choosers, '
-        '{} alternatives, and sample_size={}'
-        ).format(len(choosers), len(alternatives), SAMPLE_SIZE))
-    # filter choosers and their current choices if they point to
-    # something that isn't in the alternatives table
-    if chosenalts is not None:
-        isin = chosenalts.isin(alternatives.index)
-        try:
-            removing = isin.value_counts().loc[False]
-        except Exception:
-            removing = None
-        if removing:
-            logger.info((
-                "Removing {} choice situations because chosen "
-                "alt doesn't exist"
-            ).format(removing))
-            choosers = choosers[isin]
-            chosenalts = chosenalts[isin]
-
-    numchoosers = choosers.shape[0]
-    numalts = alternatives.shape[0]
-
-    # TODO: this is currently broken in a situation where
-    # SAMPLE_SIZE >= numalts. That may not happen often in
-    # practical situations but it should be supported
-    # because a) why not? and b) testing.
-    alts_idx = np.arange(len(alternatives))
-    if SAMPLE_SIZE < numalts:
-        # TODO: Use stdlib random.sample to individually choose
-        # alternatives for each chooser (to avoid repeatedly choosing the
-        # same alternative).
-        # random.sample is much faster than np.random.choice.
-        # [Also, it looks like this could potentially sample the same alternative more
-        #  than once for a single choice scenario, which is not ideal. -Sam]
-        sample = np.random.choice(alts_idx, SAMPLE_SIZE * numchoosers)
-        if chosenalts is not None:
-            # replace the first row for each chooser with
-            # the currently chosen alternative.
-            # chosenalts -> integer position
-            sample[::SAMPLE_SIZE] = pd.Series(
-                alts_idx, index=alternatives.index).loc[chosenalts].values
-    else:
-        assert chosenalts is None  # if not sampling, must be simulating
-        sample = np.tile(alts_idx, numchoosers)
-
-    if not choosers.index.is_unique:
-        raise Exception(
-            "ERROR: choosers index is not unique, "
-            "sample will not work correctly")
-    if not alternatives.index.is_unique:
-        raise Exception(
-            "ERROR: alternatives index is not unique, "
-            "sample will not work correctly")
-
-    alts_sample = alternatives.take(sample).copy()
-    assert len(alts_sample.index) == SAMPLE_SIZE * len(choosers.index)
-    alts_sample['join_index'] = np.repeat(choosers.index.values, SAMPLE_SIZE)
-
-    alts_sample = pd.merge(
-        alts_sample, choosers, left_on='join_index', right_index=True,
-        suffixes=('', '_r'))
-
-    chosen = np.zeros((numchoosers, SAMPLE_SIZE))
-    chosen[:, 0] = 1
-
-    logger.debug('finish: compute MNL interaction dataset')
-    return alternatives.index.values[sample], alts_sample, chosen
